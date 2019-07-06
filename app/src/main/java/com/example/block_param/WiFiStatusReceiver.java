6a70f99334a99db838db520e31cadcf75e767936
Laloww <lalow.main@gmail.com>
2019-07-06 21:12:58 +0800

    Отслеживание смены wifi

---
+++ app/src/main/java/com/example/block_param/WiFiStatusReceiver.java
@@ -0,0 +1,23 @@
+package com.example.block_param;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.net.wifi.WifiManager;
+import android.widget.Toast;
+
+public class WiFiStatusReceiver extends BroadcastReceiver {
+    public static Context mContext;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (mContext == null) mContext = context;
+        final WifiManager wm = (WifiManager) mContext.getSystemService(mContext.WIFI_SERVICE);
+        if (intent.getBooleanExtra(wm.EXTRA_SUPPLICANT_CONNECTED, true)) {
+            Toast.makeText(context, "Wifi connected to network" , Toast.LENGTH_LONG).show();
+        }
+
+        if (!intent.getBooleanExtra(wm.EXTRA_SUPPLICANT_CONNECTED, true)) {
+            Toast.makeText(context, "Wifi disconnected from network" , Toast.LENGTH_LONG).show();
+        }
+    }
+}         
