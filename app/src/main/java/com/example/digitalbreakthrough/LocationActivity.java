c1ef4156238e3c6a18fa4e6c12ceb820733afa67
blaackbirdie <vikki-rylskaya@yandex.ru>
2019-07-07 16:37:34 +0700

    drawing markers/current user point, disable bluetooth, disable network

--- app/src/main/java/com/example/digitalbreakthrough/LocationActivity.java
+++ app/src/main/java/com/example/digitalbreakthrough/LocationActivity.java
@@ -1,28 +1,36 @@
 package com.example.digitalbreakthrough;
 
 import android.Manifest;
+import android.bluetooth.BluetoothAdapter;
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.hardware.Camera;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.net.ConnectivityManager;
-import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.os.Handler;
-import android.provider.Settings;
-import android.telephony.TelephonyManager;
 import android.util.Log;
+import android.view.View;
 
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.Method;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Circle;
+import com.google.android.gms.maps.model.CircleOptions;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class LocationActivity extends FragmentActivity {
+public class LocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     public Camera camera;
     public ConnectivityManager dataManager;
@@ -66,6 +74,7 @@ public class LocationActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
 
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_SECURE_SETTINGS}, REQUEST_LOCATION);
         ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
 
         wifiManager = (WifiManager) getApplicationContext().getSystemService(Context.WIFI_SERVICE);
@@ -90,6 +99,10 @@ public class LocationActivity extends FragmentActivity {
         }, 5000);
 
         setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
 
     }
 
@@ -101,51 +114,48 @@ public class LocationActivity extends FragmentActivity {
 
         Location.distanceBetween(LatitudeWorkerPlace, LontitudeWorkerPlace, location.getLatitude(), location.getLongitude(), dist);
         float distance = dist[0];
-        if(distance < 100){
+        if (distance < 50) {
             //disable actions
 
             Log.d("test", "checkLocation: user is not far away");
             wifiManager.setWifiEnabled(false);
-            setMobileDataEnabled(false);
-            camera = Camera.open();
-
-        }
-        else
-            {
-                Log.d("test", "checkLocation: user is far away");
-                wifiManager.setWifiEnabled(true);
-                setMobileDataEnabled(true);
-                isCameraUsebyApp();
+            BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+            if (mBluetoothAdapter.isEnabled()) {
+                mBluetoothAdapter.disable();
             }
+            setMobileDataEnabled(getApplicationContext(), false);
+            //camera = Camera.open();
+
+        } else {
+            Log.d("test", "checkLocation: user is far away");
+            wifiManager.setWifiEnabled(true);
+            setMobileDataEnabled(getApplicationContext(), true);
+            isCameraUsebyApp();
+        }
     }
 
     private void getAndCheckLocation() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
-                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
 
-            }
-            else
-                {
-                    mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
-                            1000 * 10, 10, mLocationListener);
+        } else {
+            mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                    1000 * 10, 10, mLocationListener);
                     /*mLocationManager.requestLocationUpdates(
                             LocationManager.NETWORK_PROVIDER, 1000 * 10, 10,
                             mLocationListener);*/
-                    Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-                    if(location != null)
-                    {
+            if (location != null) {
 
-                        Log.d("location","lan " + location.getLatitude() + " lon: " + location.getLongitude());
-                        checkLocation(location);
-                    }
-                    else
-                        {
-                            Log.d("getAndCheckLocation", "getAndCheckLocation: Unable to track your location");
-                        }
+                Log.d("location", "lan " + location.getLatitude() + " lon: " + location.getLongitude());
+                checkLocation(location);
+            } else {
+                Log.d("getAndCheckLocation", "getAndCheckLocation: Unable to track your location");
+            }
 
-                }
+        }
 
 
     }
@@ -161,17 +171,68 @@ public class LocationActivity extends FragmentActivity {
         }
     }
 
-    private void setMobileDataEnabled(boolean isEnabled) {
-        NetworkInfo networkInfo = dataManager.getActiveNetworkInfo();
+    private void setMobileDataEnabled(Context context, boolean isEnabled) {
+        /*NetworkInfo networkInfo = dataManager.getActiveNetworkInfo();
         if(networkInfo!= null && networkInfo.isConnected() && !isEnabled)
         {
-            Settings.System.putInt(this.getContentResolver(),Settings.Global.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
+            // toggle airplane mode
+            Settings.System.putInt(
+                    getContentResolver(),
+                    Settings.Global.AIRPLANE_MODE_ON, 1);
+
+            // Post an intent to reload
+            //Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+            //intent.putExtra("state", !isEnabled);
+            //sendBroadcast(intent);
         }
         else
             {
-                Settings.System.putInt(this.getContentResolver(),Settings.System.AIRPLANE_MODE_ON, isEnabled ? 0 : 1);
-            }
-
+                Settings.System.putInt(this.getContentResolver(),Settings.System.AIRPLANE_MODE_ON, 0);
+            }*/
+        /*int value = isEnabled ? 1 : 0;
+        if (isEnabled){
+            value = 1;
+        }
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN_MR1) { //if less than verson 4.2
+            Settings.System.putInt(
+                    getContentResolver(),
+                    Settings.System.AIRPLANE_MODE_ON, value);
+        } else {
+            Settings.Global.putInt(
+                    getContentResolver(),
+                    Settings.Global.AIRPLANE_MODE_ON, value);
+        }*/
+        // broadcast an intent to inform
+        //Intent intent = new Intent(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        //intent.putExtra("state", !isEnabled);
+        //sendBroadcast(intent);
     }
 
+    @Override
+    public void onMapReady(GoogleMap map) {
+        GoogleMap mMap = map;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, REQUEST_LOCATION);
+
+        } else {
+        Location location = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        LatLng loc = new LatLng(location.getLatitude(), location.getLongitude());
+        LatLng restrictedPosition = new LatLng(LatitudeWorkerPlace,LontitudeWorkerPlace);
+
+            mMap.addMarker(new MarkerOptions().position(loc).title("Your Position"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(loc));
+
+            CircleOptions co = new CircleOptions();
+            co.center(restrictedPosition);
+            co.radius(60);
+            co.fillColor(Color.BLUE);
+            co.strokeColor(0x30ff0000);
+            co.strokeWidth(2.0f);
+
+            mMap.addCircle(co);
+        }
+    }
 }
+
+
