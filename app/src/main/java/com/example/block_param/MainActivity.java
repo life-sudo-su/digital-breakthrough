e338bb7af7ec8cf65c472c69ef8ba365cba1090f
raldenprog <raldenprog@mail.ru>
2019-07-07 09:18:20 +0700

    тестовый сервис

--- app/src/main/java/com/example/block_param/MainActivity.java
+++ app/src/main/java/com/example/block_param/MainActivity.java
@@ -1,18 +1,34 @@
 package com.example.block_param;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActivityManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, ServiceDetectCamera.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, ServiceDetectCamera.class));
     }
 }
 
